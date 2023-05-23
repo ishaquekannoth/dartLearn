7e36d5ec98052648b3523ebab80d0252796bba63
ishaque <ishaque@mobikasa.com>
2023-05-23 17:51:28 +0530

    stream_transformer

--- file1.dart
+++ file1.dart
@@ -1,12 +1,54 @@
 import 'dart:async';
 
 void main() async {
-  StreamController controller = new StreamController<String>();
-  controller.sink.add("Helllo");
-  controller.sink.add("World");
+  List<String> name = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N"
+  ];
 
-  await for (String word in controller.stream) {
-    print(word);
+  // await for (String name in Stream<String>.fromIterable(name)) {
+  //   print(name.toLowerCase());
+  // }
+
+  // await for (String name in Stream<String>.fromIterable(name)
+  //     .map((event) => event.toUpperCase())) {
+  //   print(name);
+  // }
+
+  await for (String name in Stream<String>.fromIterable(name).capitalised) {
+    print(name);
   }
-  controller.close();
+  await for (String name
+      in Stream<String>.fromIterable(name).smallCasedUsingMap) {
+    print(name);
+  }
+}
+
+class ToUpperCase extends StreamTransformerBase<String, String> {
+  @override
+  Stream<String> bind(Stream stream) {
+    return stream.map((event) => event.toUpperCase());
+  }
+}
+
+extension Capitalize on Stream<String> {
+  Stream<String> get capitalised {
+    return this.transform(ToUpperCase());
+  }
+
+  Stream<String> get smallCasedUsingMap {
+    return this.map((event) => event.toLowerCase());
+  }
 }
