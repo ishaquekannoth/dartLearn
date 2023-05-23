1ea50c4ec5facc2f3519255615bbe411725c15c4
ishaque <ishaque@mobikasa.com>
2023-05-23 14:28:42 +0530

    stream controller

--- file1.dart
+++ file1.dart
@@ -1,27 +1,12 @@
+import 'dart:async';
+
 void main() async {
-  Stream<String> maleNames() async* {
-    yield "Ishaque";
-    await Future.delayed(Duration(seconds: 1));
-    yield "Irfan";
-    await Future.delayed(Duration(seconds: 1));
-    yield "Ihsan";
-  }
+  StreamController controller = new StreamController<String>();
+  controller.sink.add("Helllo");
+  controller.sink.add("World");
 
-  Stream<String> femaleNames() async* {
-    yield "Imana";
-    await Future.delayed(Duration(seconds: 1));
-    yield "Inaya";
-    await Future.delayed(Duration(seconds: 1));
-    yield "Iqra";
+  await for (String word in controller.stream) {
+    print(word);
   }
-
-  Stream<String> allNames() async* {
-    yield* maleNames();
-    await Future.delayed(Duration(seconds: 3));
-    yield* femaleNames();
-  }
-
-  await for (String name in allNames()) {
-    print(name);
-  }
+  controller.close();
 }
