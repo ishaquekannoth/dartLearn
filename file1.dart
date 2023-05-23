7916e6a793372cc65c18ce4540eb631e3bd84ff4
ishaque <ishaque@mobikasa.com>
2023-05-23 13:34:51 +0530

    reduce

--- file1.dart
+++ file1.dart
@@ -1,10 +1,12 @@
 void main() async {
-  Stream<String> getCharactor(String name) async* {
-    for (int i = 0; i < name.length; i++) {
-      await Future.delayed(Duration(seconds: 2));
-      yield name[i];
+  Stream<double> getAllAges() async* {
+    for (int i = 1; i <= 10; i++) {
+      await Future.delayed(Duration(microseconds: 1));
+      yield i * 10;
     }
   }
+
+  double add(double a, double b) => a + b;
 
   Stream<String> getNames() async* {
     await Future.delayed(Duration(seconds: 1));
@@ -15,9 +17,11 @@ void main() async {
     yield "Ishaque";
   }
 
-  await for (String charactors
-      in getNames().asyncExpand((name) => getCharactor(name))) {
-    print(charactors);
-  }
-  ;
+  final result = await getAllAges().reduce((pre, curr) {
+    print("Previous is $pre");
+    print("Current is $curr");
+    print("Iteration done");
+    return add(pre / 10, curr / 10);
+  });
+  print(result);
 }
