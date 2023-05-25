449be7f850a6f5db2f3453e7fab0c9aa49e0988e
ishaque <ishaque@mobikasa.com>
2023-05-25 17:36:09 +0530

    Timer based streams and exceptions

--- file1.dart
+++ file1.dart
@@ -1,46 +1,66 @@
 import 'dart:async';
 
 void main() async {
-  Future<void> nonBroadCastStreamExample() async {
-    final controller = StreamController<String>();
-    controller.sink.add("Ishaque");
-    controller.sink.add("Bill Gates");
-    controller.sink.add("Elon Musk");
+  Stream<String> getNames() async* {
+    yield "A";
+    await Future.delayed(Duration(seconds: 1));
+    yield "B";
+    await Future.delayed(Duration(seconds: 2));
+    yield "C";
+    await Future.delayed(Duration(seconds: 1));
+    yield "D";
+    await Future.delayed(Duration(seconds: 4));
+    yield "E";
+    await Future.delayed(Duration(seconds: 7));
+    yield "F";
+  }
 
-    try {
-      await for (var name in controller.stream) {
-        print(name);
-        await for (var name in controller.stream) {
-          print(name);
-        }
-      }
-    } catch (e) {
-      print(e);
+  try {
+    await for (var name in getNames().streamTimeWait(Duration(seconds: 3))) {
+      print(name);
     }
+  } on TimerRanoutException catch (e) {
+    print(e.message.toString());
   }
-
-  Future<void> broadCastStreamExample() async {
-    final StreamController<String> controller = StreamController<String>.broadcast();
-
-    final sub1 = controller.stream.listen((event) {
-      print("sub1: $event");
-    });
-    final sub2 = controller.stream.listen((event) {
-      print("sub2: $event");
-    });
-    controller.sink.add("Ishaque");
-    controller.sink.add("Bill Gates");
-    //await Future.delayed(Duration(seconds: 2));
-    //sub1.cancel();
-    controller.sink.add("Elon Musk");
-    controller.close();
-
-    controller.onCancel = () {
-      print("on cancel called");
-      sub1.cancel();
-      sub2.cancel();
-    };
-  }
-
-  await broadCastStreamExample();
 }
+
+class TimeOutBeetweenEvents<T> extends StreamTransformerBase<T, T> {
+  Duration duration;
+  StreamController<T>? controller;
+  StreamSubscription<T>? subscription;
+  Timer? timer;
+  TimeOutBeetweenEvents(this.duration);
+  @override
+  Stream<T> bind(Stream<T> stream) {
+    controller = StreamController<T>(
+      onListen: () {
+        subscription = stream.listen((data) {
+          timer?.cancel();
+          timer = Timer.periodic(duration, (timer) {
+            controller?.addError(TimerRanoutException(
+              "Timer ran out",
+            ));
+          });
+          controller?.add(data);
+        }, onError: controller?.addError, onDone: controller?.close);
+      },
+      onCancel: () {
+        subscription?.cancel();
+        timer?.cancel();
+      },
+    );
+    return controller!.stream;
+  }
+}
+
+extension TimerBetweeenEvents<T> on Stream<T> {
+  Stream<T> streamTimeWait(Duration duration) =>
+      transform(TimeOutBeetweenEvents(duration));
+}
+
+class TimerRanoutException implements Exception {
+  String message;
+  TimerRanoutException(this.message);
+}
+
+
