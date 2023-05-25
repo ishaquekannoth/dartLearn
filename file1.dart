92cb4be516973329ce9d077163438a940181e7ee
ishaque <ishaque@mobikasa.com>
2023-05-25 13:00:19 +0530

    asyncMap for streams to convert as futures

--- file1.dart
+++ file1.dart
@@ -1,46 +1,27 @@
 import 'dart:async';
 
 void main() async {
+  Future<List<String>> extractCharactors(String word) async {
+    final charactors = <String>[];
+
+    for (String char in word.split("")) {
+      await Future.delayed(
+          Duration(microseconds: 100), () => charactors.add(char));
+    }
+    return charactors;
+  }
+
   Stream<String> getNames() async* {
     yield "Ishaque";
     yield "Bill Gates";
     yield "Elon Musk";
-    throw "All out of names";
   }
 
-  await for (String name in getNames().absorbErrUsingHandleError()) {
-    print(name);
-  }
-  await for (String name in getNames().absorbUsingHandlers()) {
-    print(name);
-  }
-  await for (String name in getNames().absorbUsingTransformer()) {
-    print(name);
-  }
-}
-
-extension AbsorbErrors<T> on Stream<T> {
-  Stream<T> absorbErrUsingHandleError() => handleError((error, stackTrace) {
-        print("Error occured");
-      });
-  Stream<T> absorbUsingHandlers() => transform(StreamTransformer.fromHandlers(
-        handleError: (error, stackTrace, sink) => sink.close(),
-      ));
-  Stream<T> absorbUsingTransformer() => transform(StreamErrAbsorber());
-}
-
-class StreamErrAbsorber<T> extends StreamTransformerBase<T, T> {
-  @override
-  Stream<T> bind(Stream<T> stream) {
-    final controller = StreamController<T>();
-
-    stream.listen(
-      (event) {
-        controller.sink.add(event);
-      },
-      onError: (_) {},
-      onDone: () => controller.close(),
-    );
-    return controller.stream;
-  }
+  final result = await getNames()
+      .asyncMap((event) => extractCharactors(event))
+      .fold("", (previous, element) {
+    final elements = element.join("-");
+    return "$previous  $elements";
+  });
+  print(result);
 }
