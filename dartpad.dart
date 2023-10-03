8b5d6e7d8b3a9171f61caa1306072e6d4b43c327
ishaque <ishaque@mobikasa.com>
2023-10-03 11:24:00 +0530

    cancelling of broadcast streams

--- dartpad.dart
+++ dartpad.dart
@@ -4,28 +4,35 @@ import 'dart:isolate';
 
 void main() async {
   log("From main isolate");
-  runOnIsoloate().then((result) {
+  final isolateFuture = runOnIsoloate();
+  isolateFuture.whenComplete(() => log("Completed future"));
+  isolateFuture.then((result) {
     log("Completed Spawn isolate with result: $result");
   });
+
   for (int i = 0; i < 10; i++) {
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(Duration(milliseconds: 500));
     log("Main Isolate Printing  " + i.toString());
   }
 }
 
 Future<int> runOnIsoloate() async {
   Completer<int> completer = Completer();
-  ReceivePort rp = ReceivePort();
-  Isolate current = await Isolate.spawn(workerFunction, rp.sendPort);
-  StreamSubscription? subscription;
+  ReceivePort receivePort = ReceivePort();
+  Isolate current = await Isolate.spawn(workerFunction, receivePort.sendPort);
+  final rp = receivePort.asBroadcastStream(
+    onCancel: (subscription) => subscription.cancel(),
+  );
 
+  StreamSubscription? subscription;
   subscription = rp.listen((message) {
     log("Spawn Isolate Printing  " + message.toString());
     if (message > 100) {
       completer.complete(message);
-      subscription!.cancel();
-      current.kill(priority: Isolate.immediate);
+      subscription?.cancel();
+      current.kill();
     }
+    log(completer.isCompleted.toString());
   });
 
   return completer.future;
@@ -33,9 +40,9 @@ Future<int> runOnIsoloate() async {
 
 void workerFunction(SendPort sendPort) async {
   int value = 0;
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 15; i++) {
     value = value + i;
-    await Future.delayed(Duration(milliseconds: 500));
+    await Future.delayed(Duration(milliseconds: 200));
     sendPort.send(value);
   }
 }
